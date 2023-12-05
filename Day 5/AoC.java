636799c7647ec4900635d712ef59f96f0317715d
Steve Lever <steve.leafo@gmail.com>
2023-12-05 23:04:50 +1000

    Checking in the part 2 changes

--- Day 5/AoC.java
+++ Day 5/AoC.java
@@ -6,17 +6,23 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.ArrayList;
-
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;    
 
 public class AoC {
 	
 	public static class Mapper {
-		public Integer a;
-		public Integer b;
-		public Integer c;
+		public Long a;
+		public Long b;
+		public Long c;
 	}
     public static void main(String[] args) {
-        List<Integer> seeds = new ArrayList<Integer>();
+		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+		LocalDateTime now = LocalDateTime.now();  
+		System.out.println(dtf.format(now));  
+   
+        List<Long> seeds = new ArrayList<Long>();
 		
         List<Mapper> stos = new ArrayList<Mapper>();
         List<Mapper> stof = new ArrayList<Mapper>();
@@ -26,12 +32,14 @@ public class AoC {
         List<Mapper> ttoh = new ArrayList<Mapper>();
         List<Mapper> htol = new ArrayList<Mapper>();
 
-        Path filePath = Paths.get("aoc5.txt");
+        //Path filePath = Paths.get("aoc5.txt");
+		Path filePath = Paths.get("aoc5_2.txt");
+		
         Charset charset = StandardCharsets.UTF_8;
 
 
 	    try {
-			Integer c = -1;
+			long c = -1;
 			List<String> lines = Files.readAllLines(filePath, charset);
 			for(String line: lines) {
 				if (line.length() == 0) {
@@ -50,7 +58,7 @@ public class AoC {
 						if (c == -1) {
 							String[] toks = line.split("\\s");
 							for (int i = 1; i < toks.length; ++i) {
-                                seeds.add(Integer.parseInt(toks[i]));
+                                seeds.add(Long.parseLong(toks[i]));
                             }
 						}
 					}
@@ -58,19 +66,19 @@ public class AoC {
 
 			}
 			
-			long lowest = Integer.MAX_VALUE;
+			long lowest = Long.MAX_VALUE;
 
             for (int i = 0; i < seeds.size(); i+=2)
             {
                 for (int j = 0; j < seeds.get(i+1); ++j) 
                 {
-                    int s = getValue(stos, seeds.get(i)+j);
-                    int f = getValue(stof, s);
-                    int w = getValue(ftow, f);
-                    int l = getValue(wtol, w);
-                    int t = getValue(ltot, l);
-                    int h = getValue(ttoh, t);
-                    int location = getValue(htol, h);
+                    long s = getValue(stos, seeds.get(i)+j);
+                    long f = getValue(stof, s);
+                    long w = getValue(ftow, f);
+                    long l = getValue(wtol, w);
+                    long t = getValue(ltot, l);
+                    long h = getValue(ttoh, t);
+                    long location = getValue(htol, h);
                     if (location < lowest)
                     {
                         lowest = location;
@@ -79,15 +87,18 @@ public class AoC {
                 }
             }
 			
-			System.out.printf("%d", lowest);
+			System.out.printf("%d\n", lowest);
 			
 		} catch (IOException ex) {
             System.out.format("I/O error: %s%n", ex);
         }
+		
+		now = LocalDateTime.now();  
+		System.out.println(dtf.format(now));  
 
     }
 	
-	public static Integer getValue(List<Mapper> i, Integer value) {
+	public static Long getValue(List<Mapper> i, Long value) {
 		for (int c = 0; c < i.size(); ++c){
 			if (value >= i.get(c).b) {
 				if (value <= i.get(c).b + i.get(c).c){
@@ -102,9 +113,9 @@ public class AoC {
 	public static void pump(String[] splits, List<Mapper> themap){
         Mapper m = new Mapper();   
 		
-        m.a = Integer.parseInt(splits[0]);
-        m.b = Integer.parseInt(splits[1]);
-        m.c = Integer.parseInt(splits[2]);
+        m.a = Long.parseLong(splits[0]);
+        m.b = Long.parseLong(splits[1]);
+        m.c = Long.parseLong(splits[2]);
 
         themap.add(m);
     }
